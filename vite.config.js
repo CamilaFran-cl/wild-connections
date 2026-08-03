5c77cfb2bf537d768aa9365f77c9ed3ac2b07369
nanocaceres <mimoesquisofrenia@gmail.com>
2026-08-03 19:06:40 -0400

    chore: change outDir to build to avoid Vercel dist conflicts

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   root: 'src',
   envDir: __dirname,
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: resolve(__dirname, 'build'),
     emptyOutDir: true,
     rollupOptions: {
       input: {
